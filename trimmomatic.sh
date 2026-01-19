c0ede437eb837cfa681556f07f770950058c2b8c
hoellerp <pauli.hoeller@gmail.com>
2026-01-19 17:32:49 +0100

    Update trimmomatic.sh

--- trimmomatic.sh
+++ trimmomatic.sh
@@ -22,5 +22,8 @@ outDir="$wd/results/trimmomatic"
 echo "Started at $(date)"
 mkdir -p $outDir
 
+# I used PE for paired reads
+# I used -summary to create a summary in the specified directory
+# I used -baseout to specify a base file name from which the names of the resulting files are to be derived
 trimmomatic PE -threads 4 -summary $outDir/ERR13719748_summary.txt $wd/ERR13719748_1.fastq $wd/ERR13719748_2.fastq -baseout $outDir/ERR13719748_ ILLUMINACLIP:/lisc/data/scratch/course/2025w300106/hoeller/trim_adapters.fa:2:30:10 SLIDINGWINDOW:4:20 MINLEN:75
 echo "Finished at $(date)"
