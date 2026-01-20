9c63c970cd20844325a606e5d93b7cc6d1dd109b
hoellerp <pauli.hoeller@gmail.com>
2026-01-20 17:44:54 +0100

    Update featureCount_newAnno.sh

--- featureCount_newAnno.sh
+++ featureCount_newAnno.sh
@@ -21,9 +21,13 @@ gtfFile="/lisc/data/scratch/course/2025w300106/hoeller/filtered_gene_models.gtf"
 bamFile="/lisc/data/scratch/course/2025w300106/hoeller/results/map/trimmed/filtered.bam"
 
 ### EXECUTION
-# I used -p for paired ends
-# I used --countReadPairs to count only one read per template
-# I used --primary to only count primary alignments
-# I used --minOverlap 3 to have only count reads with at least 3 overlapping bps
-# I believe this is a reverse stranded library
+# I used '-p' for paired-end reads
+# I used '--countReadPairs' to count only one read per template
+# I used '-C' to NOT count reads with ends on different chromosomes -> chimeric reads
+# I used '-T' to specify the number of CPU threads
+# I used '-s 2' to specify reversely stranded read counting
+# I used '--minOverlap 3' to have only count reads with at least 3 overlapping bps
+# I used '--primary' to only count primary alignments
+# I used '-a' to specify the location/name of the annotation (.gtf) file
+# I used '-o' to specify the name of the output file
 featureCounts -p --countReadPairs -C -T 4 -s 2 --minOverlap 3 --primary -a $gtfFile -o ERR13719748.counts.newAnno.tsv $bamFile
