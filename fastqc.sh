81147cc41472eca77e0d217d340c83ba00d98d2a
hoellerp <pauli.hoeller@gmail.com>
2026-01-20 17:05:56 +0100

    Update fastqc.sh

--- fastqc.sh
+++ fastqc.sh
@@ -18,6 +18,9 @@ outDir="/lisc/data/scratch/course/2025w300106/hoeller/results/fastqc"
 readsDir="/lisc/data/scratch/course/2025w300106/hoeller"
 
 ### EXECUTION
+# I first used 'mkdir' to create the output directory
+# I used '-t' to specify the number of threads to be used for the task
+# I used '-o' to specify the output directory into which all output files will be put
 echo "Started job at $(date)"
 mkdir $outDir
 fastqc -t 2 -o $outDir $readsDir/ERR13719748_1.fastq $readsDir/ERR13719748_2.fastq
