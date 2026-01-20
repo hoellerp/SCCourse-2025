ac6edfc66354a20c2f1c1f635620fc7c74f1c40d
hoellerp <pauli.hoeller@gmail.com>
2026-01-20 18:09:24 +0100

    Update cellranger.sh

--- cellranger.sh
+++ cellranger.sh
@@ -23,6 +23,16 @@ od="${wd}/hoeller/results/cellranger"
 id="blastula_12h"
 
 ### EXECUTION
+# I used 'mkdir -p ${od}' to create the output directory
+# I used 'cd ${od}' to move into the output directory (since CellRanger puts the files into the current directory)
+# I used '--transcriptome' to specify the location of our transcriptome reference
+# I used '--fastqs' to specify the directory of the fastq files to be used
+# I used '--force-cells 10000' to force CellRanger to end up with 10000 cells (regardless of quality)
+# I used '--nosecondary' to disable secondary analysis
+# I used '-- localcores' to specify the max. number of CPUs to use for the task
+# I used '--sample'to specify the prefix of the filenames in the fastq files which are to be used
+# I used '--id' to specify the name of the output folder
+# I used --create-bam false' so that no BAM file will be generated
 mkdir -p ${od}
 cd ${od}
 cellranger count --transcriptome $index --fastqs $fastq_dir --force-cells 10000 --nosecondary --localcores 16 --sample $sample --id $id --create-bam false
